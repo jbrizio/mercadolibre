92579b0f7af6d9e55eb4eca910c5eaf266e6cfaf
jbrizio <Jonathan.X.Brizio.-ND@disney.com>
2016-04-19 11:10:04 -0300

    First upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Information
+To visit the demo online, please access to:
+http://jbrizio.github.io/mercadolibre/
---
+++ src/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// Carousel
+var carousel = $('.myCarousel').carousel({ 
+    'pagination': true 
+});
+
+// Tabs
+var tabs = $('#myTab').tabs();
+
+// Show the second tab
+tabs.show(2);
